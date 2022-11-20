c53a53e8410ab0489c63a09e2931d0de5eca7b9e
Joe Desmond <joseph.desmond@grabcad.com>
2022-11-19 20:43:01 -0500

    Added base58check and test commands

--- tsengcoin-core/src/top_level_commands.rs
+++ tsengcoin-core/src/top_level_commands.rs
@@ -1,6 +1,6 @@
 use std::{collections::HashMap, error::Error};
 
-use crate::{command::{CommandMap, Command, CommandInvocation, Field, FieldType}, tsengscript_interpreter::{execute, ExecutionResult, Token}};
+use crate::{command::{CommandMap, Command, CommandInvocation, Field, FieldType}, tsengscript_interpreter::{execute, ExecutionResult, Token}, wallet::{address_from_public_key, address_to_b58c, b58c_to_address}};
 
 fn run_script(_command_name: &String, invocation: &CommandInvocation, _state: Option<()>) -> Result<(), Box<dyn Error>> {
     let script = invocation.get_field("script").unwrap();
@@ -21,6 +21,35 @@ fn run_script(_command_name: &String, invocation: &CommandInvocation, _state: Op
     Ok(())
 }
 
+fn random_test_address(_command_name: &String, _invocation: &CommandInvocation, _state: Option<()>) -> Result<(), Box<dyn Error>> {
+    let rand_bytes: [u8; 32] = rand::random();
+    let address = address_from_public_key(&rand_bytes.to_vec());
+
+    println!("{}", hex::encode(&address));
+
+    Ok(())
+}
+
+fn b58c_encode(_command_name: &String, invocation: &CommandInvocation, _state: Option<()>) -> Result<(), Box<dyn Error>> {
+    let raw = invocation.get_field("hex-bytes").unwrap();
+    let bytes = hex::decode(raw)?;
+    let encoded = address_to_b58c(&bytes);
+
+    println!("{}", encoded);
+
+    Ok(())
+}
+
+fn b58c_decode(_command_name: &String, invocation: &CommandInvocation, _state: Option<()>) -> Result<(), Box<dyn Error>> {
+    let raw = invocation.get_field("encoded").unwrap();
+    let decoded = b58c_to_address(raw)?;
+    let hex_str = hex::encode(decoded);
+
+    println!("{}", hex_str);
+
+    Ok(())
+}
+
 pub fn make_command_map() -> CommandMap<()> {
     let mut out: CommandMap<()> = HashMap::new();
     let run_script_cmd: Command<()> = Command {
@@ -29,8 +58,27 @@ pub fn make_command_map() -> CommandMap<()> {
             Field::new("script", FieldType::Spaces(0))
         ]
     };
+    let random_test_address_cmd: Command<()> = Command {
+        processor: random_test_address,
+        expected_fields: vec![]
+    };
+    let b58c_encode_cmd: Command<()> = Command {
+        processor: b58c_encode,
+        expected_fields: vec![
+            Field::new("hex-bytes", FieldType::Pos(0))
+        ]
+    };
+    let b58c_decode_cmd: Command<()> = Command {
+        processor: b58c_decode,
+        expected_fields: vec![
+            Field::new("encoded", FieldType::Pos(0))
+        ]
+    };
 
     out.insert(String::from("run-script"), run_script_cmd);
+    out.insert(String::from("random-test-address"), random_test_address_cmd);
+    out.insert(String::from("b58c-encode"), b58c_encode_cmd);
+    out.insert(String::from("b58c-decode"), b58c_decode_cmd);
 
     out
 }
--- tsengcoin-core/src/wallet.rs
+++ tsengcoin-core/src/wallet.rs
@@ -1,10 +1,15 @@
+use std::error::Error;
+
 use ring::digest::{Context, SHA256};
 use ripemd::{Ripemd160, Digest};
+use base58check::{ToBase58Check, FromBase58Check, FromBase58CheckError};
 
 pub type Hash160 = [u8; 20];
 pub type Hash256 = [u8; 32];
 
 pub type Address = Hash160;
+
+const B58C_VERSION_PREFIX: u8 = 0x01;
 
 pub fn address_from_public_key(public_key: &Vec<u8>) -> Address {
     let mut context = Context::new(&SHA256);
@@ -20,4 +25,27 @@ pub fn address_from_public_key(public_key: &Vec<u8>) -> Address {
     out.copy_from_slice(&result);
 
     out
+}
+
+pub fn address_to_b58c(address: &Vec<u8>) -> String {
+    address.to_base58check(B58C_VERSION_PREFIX)
+}
+
+pub fn b58c_to_address(b58c: String) -> Result<Address, Box<dyn Error>> {
+    let res = b58c.from_base58check();
+
+    match res {
+        Err(FromBase58CheckError::InvalidChecksum) => Err("Invalid checksum")?,
+        Err(FromBase58CheckError::InvalidBase58(_)) => Err("Invalid base58")?,
+        Ok((version, _)) if version != B58C_VERSION_PREFIX => Err("Invalid version")?,
+        Ok((_, bytes)) => {
+            let offset = 20 - bytes.len();
+            let mut out = [0 as u8; 20];
+
+            // Pad 20-byte integer with correct number of zeros
+            out[offset..].copy_from_slice(&bytes);
+
+            Ok(out)
+        },
+    }
 }
