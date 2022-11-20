7dbcc3a6bf8a49b8aeb47814e30a09a1f5eb23bb
Joe Desmond <joseph.desmond@grabcad.com>
2022-11-19 23:34:18 -0500

    Improved command API and added help commands

--- tsengcoin-core/src/command.rs
+++ tsengcoin-core/src/command.rs
@@ -1,9 +1,12 @@
+use std::cmp::max;
 use std::collections::HashMap;
 use std::error::Error;
 
 pub struct Command<T> {
     pub processor: CommandProcessor<T>,
-    pub expected_fields: Vec<Field>
+    pub expected_fields: Vec<Field>,
+    pub flags: Vec<Flag>,
+    pub desc: String
 }
 
 pub type CommandProcessor<T> = fn (command_name: &String, invocation: &CommandInvocation, state: Option<T>) -> Result<(), Box<dyn Error>>;
@@ -39,8 +42,16 @@ impl CommandInvocation {
 pub struct Field {
     pub name: String,
     pub field_type: FieldType,
+    pub desc: String,
 }
 
+#[derive(Clone)]
+pub struct Flag {
+    pub name: String,
+    pub desc: String,
+}
+
+#[derive(PartialEq)]
 pub enum FieldType {
     /// A "var" must be passed in as a named variable with --name=value syntax
     Var,
@@ -56,8 +67,14 @@ pub enum FieldType {
 }
 
 impl Field {
-    pub fn new(name: &str, field_type: FieldType) -> Self {
-        Field { name: name.to_owned(), field_type }
+    pub fn new(name: &str, field_type: FieldType, desc: &str) -> Self {
+        Field { name: name.to_owned(), field_type, desc: desc.to_owned() }
+    }
+}
+
+impl Flag {
+    pub fn new(name: &str, desc: &str) -> Self {
+        Flag { name: name.to_owned(), desc: desc.to_owned() }
     }
 }
 
@@ -68,6 +85,17 @@ pub fn dispatch_command<T>(args: &Vec<String>, map: &CommandMap<T>, state: Optio
     }
 
     let cmd_name = &args[0];
+
+    if cmd_name == "help" {
+        if args.len() < 2 {
+            help(map);
+        } else {
+            let cmd_name = args[1].clone();
+            help_cmd(map, cmd_name);
+        }
+
+        return;
+    }
 
     let command = match map.get(cmd_name) {
         Some(obj) => obj.to_owned(),
@@ -114,7 +142,7 @@ fn decompose_raw_args(raw_args: &Vec<String>, expected_fields: &Vec<Field>) -> R
 
     // Process explicitly assigned fields first and recalculate positions for
     // positional fields
-    for Field {name, field_type} in expected_fields {
+    for Field {name, field_type, desc} in expected_fields {
         // Will only be Some if the field was assigned with `--name=value` syntax
         let var_field = assignments.get(name).cloned();
 
@@ -122,13 +150,13 @@ fn decompose_raw_args(raw_args: &Vec<String>, expected_fields: &Vec<Field>) -> R
             (FieldType::Var, Some(var)) => drop(fields.insert(name.to_owned(), var)),
             (FieldType::Var, None) => return Err(format!("Missing expected argument {name}. Pass this in with --{name}=<value>"))?,
             (FieldType::Pos(_) | FieldType::Spaces(_), Some(var)) => drop(fields.insert(name.to_owned(), var)),
-            (FieldType::Pos(_), None) => pos_fields.push(Field::new(name, FieldType::Pos(pos_fields.len()))),
-            (FieldType::Spaces(_), None) => pos_fields.push(Field::new(name, FieldType::Spaces(pos_fields.len())))
+            (FieldType::Pos(_), None) => pos_fields.push(Field::new(name, FieldType::Pos(pos_fields.len()), desc)),
+            (FieldType::Spaces(_), None) => pos_fields.push(Field::new(name, FieldType::Spaces(pos_fields.len()), desc))
         }
     }
 
     // Now go through the remaining ordered arguments with new positions and pick them out
-    for Field {name, field_type} in pos_fields {
+    for Field {name, field_type, desc: _} in pos_fields {
         match field_type {
             FieldType::Var => unreachable!(),
             FieldType::Pos(pos) if pos.to_owned() < ordered_args.len() => drop(fields.insert(name.to_owned(), ordered_args[pos.to_owned()].clone())),
@@ -146,4 +174,112 @@ fn decompose_raw_args(raw_args: &Vec<String>, expected_fields: &Vec<Field>) -> R
     };
 
     Ok(out)
+}
+
+fn help<T>(map: &CommandMap<T>) {
+    println!("These are the supported commands. Do 'help command_name' to learn more about a specific command.\n");
+    let mut keys: Vec<String> = map.keys().map(|k| k.to_owned()).collect();
+    keys.sort();
+
+    for cmd_name in keys {
+        let cmd = map.get(&cmd_name).unwrap();
+        println!("\t{}\n\t\t{}", cmd_name, cmd.desc);
+    }
+}
+
+fn help_cmd<T>(map: &CommandMap<T>, cmd_name: String) {
+    let command = match map.get(&cmd_name) {
+        Some(obj) => obj.to_owned(),
+        None => {
+            println!("Unrecognized command: {cmd_name}");
+            return;
+        }
+    };
+
+    println!("{}\n", command.desc);
+    println!("Syntax: \t{}", make_syntax_string(&cmd_name, &command));
+
+    let (vars, mut poses): (Vec<&Field>, Vec<&Field>) = 
+        command.expected_fields
+            .iter()
+            .partition(|f| f.field_type == FieldType::Var);
+
+    poses.sort_by_key(|f| {
+        match f.field_type {
+            FieldType::Pos(pos) => pos,
+            FieldType::Spaces(pos) => pos,
+            _ => unreachable!()
+        }
+    });
+    
+    let mut var_names: Vec<(String, String)> = 
+        vars
+            .iter()
+            .map(|f| (f.name.to_owned(), f.desc.to_owned()))
+            .collect();
+
+    var_names.sort();
+
+    let mut flags = command.flags.clone();
+    flags.sort_by_key(|f| f.name.clone());
+
+    if poses.len() > 0 {
+        println!("\nRequired arguments:\n");
+
+        for field in poses {
+            println!("\t{}\n\t\t{}", field.name, field.desc);
+        }
+    } else {
+        println!("\nThere are no required positional arguments");
+    }
+
+    if var_names.len() > 0 {
+        println!("\nRequired keyword arguments:\n");
+
+        for (name, desc) in var_names {
+            println!("\t--{name}\n\t\t{desc}");
+        }
+    }
+
+    if flags.len() > 0 {
+        println!("\nOptional flags:\n");
+
+        for Flag { name, desc } in flags {
+            println!("\t--{name}\n\t\t{desc}");
+        }
+    }
+}
+
+fn make_syntax_string<T>(name: &String, command: &Command<T>) -> String {
+    let mut out = String::from(name);
+    let mut max_pos: isize = -1;
+
+    for field in &command.expected_fields {
+        match field.field_type {
+            FieldType::Pos(pos) => max_pos = max(max_pos, pos.to_owned().try_into().unwrap()),
+            FieldType::Spaces(pos) => max_pos = max(max_pos, pos.to_owned().try_into().unwrap()),
+            FieldType::Var => (),
+        };
+    }
+
+    let mut names: Vec<String> = Vec::with_capacity((max_pos + 1).try_into().unwrap());
+    for _ in 0..names.capacity() {
+        names.push(String::from(""));
+    }
+    
+    for Field { name, field_type, desc: _ } in &command.expected_fields {
+        match field_type {
+            FieldType::Pos(pos) => names[pos.to_owned()] = name.to_owned(),
+            FieldType::Spaces(pos) => names[pos.to_owned()] = name.to_owned(),
+            FieldType::Var => ()
+        };
+    }
+
+    for name in names {
+        out.push_str(" <");
+        out.push_str(&name);
+        out.push_str(">");
+    }
+
+    out
 }
--- tsengcoin-core/src/top_level_commands.rs
+++ tsengcoin-core/src/top_level_commands.rs
@@ -2,7 +2,7 @@ use std::{collections::HashMap, error::Error};
 
 use ring::signature::KeyPair;
 
-use crate::{command::{CommandMap, Command, CommandInvocation, Field, FieldType}, tsengscript_interpreter::{execute, ExecutionResult, Token}, wallet::{address_from_public_key, address_to_b58c, b58c_to_address, create_keypair, load_keypair}};
+use crate::{command::{CommandMap, Command, CommandInvocation, Field, FieldType, Flag}, tsengscript_interpreter::{execute, ExecutionResult, Token}, wallet::{address_from_public_key, address_to_b58c, b58c_to_address, create_keypair, load_keypair}};
 
 fn run_script(_command_name: &String, invocation: &CommandInvocation, _state: Option<()>) -> Result<(), Box<dyn Error>> {
     let script = invocation.get_field("script").unwrap();
@@ -88,38 +88,82 @@ pub fn make_command_map() -> CommandMap<()> {
     let run_script_cmd: Command<()> = Command {
         processor: run_script,
         expected_fields: vec![
-            Field::new("script", FieldType::Spaces(0))
-        ]
+            Field::new(
+                "script",
+                FieldType::Spaces(0),
+                "Code written in TsengScript"
+            )
+        ],
+        flags: vec![
+            Flag::new("show-stack", "Print the contents of the stack when the program finishes")
+        ],
+        desc: String::from("Run a TsengScript program and see the output and stack trace")
     };
     let random_test_address_hex_cmd: Command<()> = Command {
         processor: random_test_address,
-        expected_fields: vec![]
+        expected_fields: vec![],
+        flags: vec![],
+        desc: String::from("Generate a random test TsengCoin address in hex")
     };
     let b58c_encode_cmd: Command<()> = Command {
         processor: b58c_encode,
         expected_fields: vec![
-            Field::new("hex-bytes", FieldType::Pos(0))
-        ]
+            Field::new(
+                "hex-bytes",
+                FieldType::Pos(0),
+                "The hex string to be encoded. Do not include the '0x' prefix"
+            )
+        ],
+        flags: vec![],
+        desc: String::from("Encode a hex string in base58check. The hex string is treated as a TsengCoin address")
     };
     let b58c_decode_cmd: Command<()> = Command {
         processor: b58c_decode,
         expected_fields: vec![
-            Field::new("encoded", FieldType::Pos(0))
-        ]
+            Field::new(
+                "encoded",
+                FieldType::Pos(0),
+                "A string encoded in base58check"
+            )
+        ],
+        flags: vec![],
+        desc: String::from("Decode a base58check string to hex. The encoded string is treated as a TsengCoin address")
     };
     let create_address_cmd: Command<()> = Command {
         processor: create_address,
         expected_fields: vec![
-            Field::new("keypair-path", FieldType::Pos(0)),
-            Field::new("password", FieldType::Spaces(1))
-        ]
+            Field::new(
+                "keypair-path",
+                FieldType::Pos(0),
+                "Path to a keypair file"
+            ),
+            Field::new(
+                "password",
+                FieldType::Spaces(1),
+                "Password to the given keypair file"
+            )
+        ],
+        flags: vec![],
+        desc: String::from(
+            "Create a TsengCoin address and lock it with a password. The file created by this command must be protected because it contains your private key"
+        )
     };
     let test_load_keypair_cmd: Command<()> = Command {
         processor: test_load_keypair,
         expected_fields: vec![
-            Field::new("keypair-path", FieldType::Pos(0)),
-            Field::new("password", FieldType::Spaces(1))
-        ]
+            Field::new(
+                "keypair-path",
+                FieldType::Pos(0),
+                "Path to a keypair file"
+            ),
+            Field::new(
+                "password",
+                FieldType::Spaces(1),
+                "Password to the given keypair file"
+            )
+        ],
+        flags: vec![],
+        desc: String::from("Load a keypair file locked with a password and get the address out of it. The file is encrypted so this only works if you have the right password")
     };
 
     out.insert(String::from("run-script"), run_script_cmd);
